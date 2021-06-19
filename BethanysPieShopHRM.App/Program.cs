7a9d2e74447aa5a18766999ddef8486a4421a244
seangnewman <seangnewman@msn.com>
2021-06-18 21:51:58 -0700

    Data access,  forms, DI

--- BethanysPieShopHRM.App/Program.cs
+++ BethanysPieShopHRM.App/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using BethanysPieShopHRM.App.Services;
 
 namespace BethanysPieShopHRM.App
 {
@@ -17,7 +18,12 @@ namespace BethanysPieShopHRM.App
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            //builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            //Inject HttpClientFactory
+            builder.Services.AddHttpClient<IEmployeeDataService, EmployeeDataService>(client => client.BaseAddress = new Uri("https://localhost:44340/"));
+            builder.Services.AddHttpClient<ICountryDataService, CountryDataService>(client => client.BaseAddress = new Uri("https://localhost:44340/"));
+            builder.Services.AddHttpClient<IJobCategoryDataService, JobCategoryService>(client => client.BaseAddress = new Uri("https://localhost:44340/"));
 
             await builder.Build().RunAsync();
         }
